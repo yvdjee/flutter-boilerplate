a81055b6807324c5c17b3d22d6c6745b7917928c
X-SLAYER <slayertn1999@gmail.com>
2021-09-20 11:13:01 +0100

    fix localization

--- lib/stores/language/app_localization.dart
+++ lib/stores/language/app_localization.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppLocalization extends ChangeNotifier {
-  Locale _locale = Locale('ar');
-  String selectedLanguageCode = 'ar';
+  Locale _locale = Locale('fr');
+  String selectedLanguageCode = 'fr';
 
   Locale get getLocale => _locale;
 
   toggleLocal(Locale locale) {
     this._locale = locale;
-    this.selectedLanguageCode = "ar";
+    this.selectedLanguageCode = locale.toLanguageTag();
     notifyListeners();
   }
 }
